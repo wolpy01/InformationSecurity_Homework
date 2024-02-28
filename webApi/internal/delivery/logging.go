2ee27b224581cb4381e1d87371e82b73ca8c7649
vfomenko <vladislav.fomenko.2014@gmail.com>
2024-02-28 13:23:41 +0300

    Обновлено ReadMe

--- webApi/internal/delivery/logging.go
+++ webApi/internal/delivery/logging.go
@@ -9,6 +9,7 @@ func Log(upstream http.Handler) http.Handler {
 	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
 		log.Println(r.Method, r.Host, r.URL.Path)
 		r.Header.Del("If-Modified-Since")
+		r.Header.Del("If-None-Match")
 		upstream.ServeHTTP(w, r)
 	})
 }
